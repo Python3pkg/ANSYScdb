149fbef1c2c276862464afe586fa63491bcc1dd7
Alex <akascap@gmail.com>
2016-09-09 11:23:17 +0200

    included wheel support

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages = ['ANSYScdb', 'ANSYScdb.Tests'],
 
     # Version
-    version='0.11.2',
+    version='0.11.3',
 
     description='Loads ANSYS cdb files',
     long_description=open('README.rst').read(),
